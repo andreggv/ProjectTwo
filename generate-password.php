31d00591d9fe55a8dba4b6c710d21ef1af8c3def
Andre Gonzalez <andreggv@gmail.com>
2014-07-21 03:01:38 -0400

    first commit to test PHP

---
+++ generate-password.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        
+        <! Metas>
+        
+        <meta charset="utf-8"/>
+        <meta name="robots" content ="nofollow"/>
+        <meta http-equiv="pragma" content="no-cache" />
+        
+        <! Visual>
+        
+        <link rel="stylesheet" type="text/css" href="style-standard.css">
+        <title>LeSphnix: atumatic generate easy to remember and hard to break passwords</title>
+        
+    </head>
+    <body>
+       
+        
+        <?php //generate-password.php
+        
+        init_set('display_erros',1);
+        $Number-Of-Words = $_POST['num-words'];
+        $Favourite-Separator = $_POST['symbol-separator'];
+        $Capitals = $_POST['capitals'];
+
+        echo $Capitals;   
+            
+        ?>
+        
+    </body>
+    
+</html>
